0cb7ffdcab7f0854fef1990a3e18a65a87b6f9ba
yunzoo <115973953+yunzoo@users.noreply.github.com>
2026-06-11 08:50:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,12 @@ document.addEventListener("DOMContentLoaded", function () {
             "img/about_hq_exterior.jpg",
             "img/about_vn_exterior.jpg",
             "img/about_hq_production.jpg",
-            "img/prod_roll_1.jpg"
+            "prod_process_0.jpg"
+            "img/main_bg1.jpg",
+            "img/main_bg2.jpg",
+            "img/main_bg3.jpg",
+            "img/main_bg4.jpg", 
+            "img/main_bg5.jpg
         ];
         let heroIndex = 0;
         let slideInterval;
